593206f455c46d5b41e4212b26df4196463a2cdb
LucasJuvito <lucasjuvito@gmail.com>
2021-10-10 16:59:01 -0300

    Resolvido problema dos link nos botões só funcionarem em cima das palvaras e não do botão todo

--- site_MercadoFechado/js/index.js
+++ site_MercadoFechado/js/index.js
@@ -15,7 +15,7 @@ function ObterCategorias(onsuccess, onerror) {
 
 ObterCategorias((categorias) => {
     var divCategorias = document.getElementById("categorias-lista");
-    var base = '<button class="btn_categorias" id="id_categoria"><a href="anuncios.html?idcategoria=id_categoria&categoria=nome_categoria">nome_categoria</a></button>';
+    var base = '<a href="anuncios.html?idcategoria=id_categoria&categoria=nome_categoria"><button class="btn_categorias" id="id_categoria">nome_categoria</button></a>';
     for(var i = 0; i < categorias.length; i++) {
         var elemento = replaceAll(base, "id_categoria", categorias[i].ID);
         elemento = replaceAll(elemento, "nome_categoria", categorias[i].Nome);
